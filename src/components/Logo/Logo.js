f0bf320fb85eeb80b29cab8214e3ec0ffd9c965f
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-20 16:14:37 -0700

    adding padding

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"> 
-                    <img alt='logo' src={brain}/>
+                    <img alt='logo' style={{paddtingTop: '5px'}} src={brain}/>
                 </div>
             </Tilt>
         </div>
