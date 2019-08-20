359f4ec71611e744f58c58f7b65363cb62a8c80a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-20 09:09:14 -0700

    resollving bugs

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Navigation = () => {
     return (
         <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
-            <p className="f3 link dim black underlined pa3 pointer">Sign Out</p>
+            <p>Sign Out</p>
         </nav>
     );
 }
