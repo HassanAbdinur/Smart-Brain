a3204d3743370d0a44d846c463f979cd7d8df57a
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-20 13:34:04 -0700

    fixing bugs

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,9 @@ const Logo = () => {
     return (
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-                <div className="Tilt-inner"> 👽 </div>
+                <div className="Tilt-inner"> 
+                    <span role="img" area-label="alien">👽</span> 
+                </div>
             </Tilt>
         </div>
     )
