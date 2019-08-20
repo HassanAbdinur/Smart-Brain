bde14bc63cc31390dcf94b6ac08ff1a3d7748763
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-20 15:46:47 -0700

    adding changes

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -9,7 +9,7 @@ const Logo = () => {
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"> 
-                    <span role="img" area-label="alien"><img alt='logo' src={brain}/></span> 
+                    <img alt='logo' src={brain}/>
                 </div>
             </Tilt>
         </div>
