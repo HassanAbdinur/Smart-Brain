174088a442e961065d37632ef3eae9de31beeb0f
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-20 15:24:27 -0700

    importing brain

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Tilt from 'react-tilt';
+import brain from './brain.png';
 import 'Logo.css';
 
 
@@ -8,7 +9,7 @@ const Logo = () => {
         <div className='ma4 mt0'>
             <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
                 <div className="Tilt-inner"> 
-                    <span role="img" area-label="alien">👽</span> 
+                    <span role="img" area-label="alien"><img src={brain}/></span> 
                 </div>
             </Tilt>
         </div>
