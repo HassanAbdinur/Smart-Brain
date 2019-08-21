dff868dbe5219636636831f39afd937190b08b70
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-21 00:12:19 -0700

    adding new js features

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+
+const particlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+          <Particles 
+            params={particlesOptions}
+          />
         <Navigation />
         <Logo />
         <Rank />
