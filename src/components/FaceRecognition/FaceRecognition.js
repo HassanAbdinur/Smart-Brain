57dfba73cea68305786e4826936fc811aeeb230d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-22 15:06:20 -0700

    adding new component

---
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FaceRecognition = () => {
+    return (
+        <div className='center'>
+            <img src={'https://samples.clarifai.com/face-det.jpg'} />
+        </div>
+    )
+}
+
+
+export default FaceRecognition;
