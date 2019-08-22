f0e776a76dcee9c92fbd40e0c62aab89bdbdf81b
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-22 15:20:24 -0700

    working on bugs

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,9 +3,9 @@ import React from 'react';
 const FaceRecognition = () => {
     return (
         <div className='center'>
-            <img src={'https://samples.clarifai.com/face-det.jpg'} />
+            <img alt='' src={'https://samples.clarifai.com/face-det.jpg'} />
         </div>
-    )
+    );
 }
 
 
