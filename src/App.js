1ad693009ec631a8353c6c4d91f338ddfc71f6d2
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-22 23:36:58 -0700

    adding changes

--- src/App.js
+++ src/App.js
@@ -35,6 +35,9 @@ class App extends Component {
     }
   }
 
+  calculateFaceLocation = (data) => {
+
+  }
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   } 
@@ -44,14 +47,8 @@ class App extends Component {
     app.models.predict(
       Clarifai.FACE_DETECT_MODEL,
       this.state.input)
-      .then(
-      function(response) {
-        console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
-      },
-      function(err) {
-        // there was an error
-      }
-    );
+      .then(response => this.calculateFaceLocation(response))
+      .catch(err => console.log(err));
   }
 
   render() {
