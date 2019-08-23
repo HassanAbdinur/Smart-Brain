7d70a6f29b1215ed7b98ffceae528f90cb5df015
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-23 00:06:55 -0700

    working on image detection box

--- src/App.js
+++ src/App.js
@@ -35,9 +35,23 @@ class App extends Component {
     }
   }
 
-  calculateFaceLocation = (data) => {
-
+  displayFaceBox = (box) =. {
+    this.setState({box: box});
   }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width;
+      topRow: clarifaiFace.top_row * height;
+      rightCol: width - (clarifaiFace.right_col * width);
+      bottomRow: height - (clarifaiFace.bottom_row * height);
+    }
+  }
+
   onInputChange = (event) => {
     this.setState({input: event.target.value});
   } 
