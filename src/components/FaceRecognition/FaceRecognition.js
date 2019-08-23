a6ee7c36d7ba89aa7880340dc1b5175ae8476863
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-23 00:26:52 -0700

    adding styling to bounding box

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -6,6 +6,7 @@ const FaceRecognition = ({ imageUrl }) => {
         <div className='center ma'>
             <div className='absolute mt2'>
                  <img id='inputImage' alt='' src={imageUrl} width='500px' height='auto' />
+                 <div className='bounding-box'></div>
             </div>
         </div>
     );
