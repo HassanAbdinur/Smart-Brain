930fe01c8ef0cffba73fd811b75089e1f202606d
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-23 01:03:42 -0700

    fixing some errors

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -11,7 +11,7 @@ const Signin = () => {
         <input class="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
       </div>
       <div class="mv3">
-        <label class="db fw6 lh-copy f6" for="password">Password</label>
+        <label class="db fw6 lh-copy f6" for="password" />Password</label>
         <input class="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
       </div>
       <label class="pa0 ma0 lh-copy f6 pointer"><input type="checkbox"> Remember me</label>
