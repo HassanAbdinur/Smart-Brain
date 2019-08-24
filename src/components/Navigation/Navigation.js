fad1d0ef67dacafada91b54e0e3173107ec74f7f
Hassan Abdinur <hassan.abdinur123@gmail.com>
2019-08-24 12:59:42 -0700

    adding new navigation on home

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -2,9 +2,18 @@ import React from 'react';
 
 const Navigation = ({ onRouteChange }) => {
     return (
-        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+        if (isSignedIn) {
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
             <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign Out</p>
         </nav>
+        } else {
+            <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Sign In</p>
+        </nav>
+        <nav style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p onClick={() => onRouteChange('signin')} className='f3 link dim black underline pa3 pointer'>Register</p>
+        </nav>
+        }
     );
 }
 
